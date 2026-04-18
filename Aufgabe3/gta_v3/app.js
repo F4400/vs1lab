18125618e36ae543f9acee19841f969f5d5cface
oliver <oliverhaessler@gmail.com>
2026-04-19 00:47:20 +0200

    provide startic files from public folder

--- Aufgabe3/gta_v3/app.js
+++ Aufgabe3/gta_v3/app.js
@@ -44,6 +44,10 @@ app.use(express.urlencoded({ extended: false }));
 
 // TODO: ... your code here ...
 
+// static files css, js, images, favicon.ico, index.html, error.ejs, index.ejs
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 // Set dedicated script for routing
 app.use('/', indexRouter);
 
